479e92f8cfacb2d4f5cb6a4d379edda355c9c930
Dmytro Konovaliuk <dmytro.k@cheitgroup.com>
2024-05-13 10:58:53 +0300

    text positioning

--- components/sections/hero.php
+++ components/sections/hero.php
@@ -1,12 +1,10 @@
 <section class="hero">
 	<div class="container">
-		<h2 class="h2-title h2-title_small-indent hero__text js-hero-text">
+		<h2 class="h2-title h2-title_small-indent hero__heading hero__text js-hero-text">
 			We shape the future human habitat by designing innovation today.
 		</h2>
 		<p class="hero__description hero__text js-hero-text">
-			We start collaborating with you when you have
-			just an idea and keep doing so when you are
-			a 4B dollars unicorn.cc
+			We start collaborating with you when you have just an idea and keep doing so when you are a 4B dollars unicorn.cc
 		</p>
 	</div>
 </section>
