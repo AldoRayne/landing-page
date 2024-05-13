740615f2128234704d61dce9eca553a145bea515
Dmytro Konovaliuk <dmytro.k@cheitgroup.com>
2024-05-13 12:53:12 +0300

    images hover

---
+++ components/sections/projects.php
@@ -0,0 +1,22 @@
+<section class="projects">
+	<div class="container">
+		<div class="projects__row">
+			<div class="projects__item">
+				<div class="projects__image-wrapper">
+					<img src="../assets/img/projects/Fendi-min.png" width="864" height="864" loading="lazy" alt="Fendi" class="projects__image">
+					<img src="../assets/img/projects/Fendi hover-min.png" width="864" height="864" loading="lazy" alt="Fendi" class="projects__image projects__image_hover">
+				</div>
+				<h3 class="h3-title">My Fav</h3>
+				<h4 class="h4-title">Social marketplace with image recognition shopping</h4>
+				<ul class="tags">
+					<li class="tags__item">
+						App Design
+					</li>
+					<li class="tags__item">
+						Branding
+					</li>
+				</ul>
+			</div>
+		</div>
+	</div>
+</section>
--- pages/home.php
+++ pages/home.php
@@ -5,16 +5,19 @@
 	<meta charset="UTF-8">
 	<title>Home Page</title>
 	<?php include('../components/head/_head.php') ?>
-	<link rel="stylesheet" href="../assets/css/section-preloader.min.css" />
+	<!-- <link rel="stylesheet" href="../assets/css/section-preloader.min.css" /> -->
+	<link rel="stylesheet" href="../assets/css/element-tags.min.css" />
 	<link rel="stylesheet" href="../assets/css/section-hero.min.css" />
+	<link rel="stylesheet" href="../assets/css/section-projects.min.css" />
 
 </head>
 
 <body class="overflow-hidden">
 	<?php include('../components/header/_header.php') ?>
 	<main>
-		<?php include('../components/sections/preloader.php') ?>
-		<?php include('../components/sections/hero.php') ?>
+		<!-- <?php include('../components/sections/preloader.php') ?> -->
+		<!-- <?php include('../components/sections/hero.php') ?> -->
+		<?php include('../components/sections/projects.php') ?>
 	</main>
 	<?php include('../components/footer/_footer.php') ?>
 	<?php include('../components/scripts/_scripts.php') ?>
@@ -22,8 +25,9 @@
 		/** Global gsap.timeline() value */
 		const globalTl = gsap.timeline();
 	</script>
-	<script src="../assets/js/section-preloader.js"></script>
-	<script src="../assets/js/section-hero.js"></script>
+	<!-- <script src="../assets/js/section-preloader.min.js"></script> -->
+	<script src="../assets/js/section-hero.min.js"></script>
+	<script src="../assets/js/section-projects.js"></script>
 </body>
 
 </html>
