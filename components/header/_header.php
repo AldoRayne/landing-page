1efdef4b597268f852d301537b79fa76b9ea165a
Dmytro Konovaliuk <dmytro.k@cheitgroup.com>
2024-05-14 09:24:35 +0300

    add logo

--- components/header/_header.php
+++ components/header/_header.php
@@ -1,5 +1,6 @@
 <header class="header js-header">
-	<div class="container">
+	<div class="container header__content">
+		<img src="../assets/img/logo.svg" width="150" height="32" loading="lazy" alt="logo" class="header__logo">
 		<a href="#" class="button">
 			<span class="button__text">Hire us</span>
 		</a>
